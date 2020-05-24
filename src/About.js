8b7296318ff585ca57006df48c34da533fccf42a
Colin Swinney <colinjswinney@gmail.com>
2020-05-24 15:57:32 -0500

    Removed unused files, created Home, About, Contact files and setup Router

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>Facts about me:</p>
+        <ul>
+          <li>n/a</li>
+        </ul>
+      </div>
+    );
+  }
+}
+ 
+export default About;
---
+++ src/Main.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+ 
+class Main extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+
+          <h1>Colin Swinney</h1>
+
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+ 
+export default Main;
